f57ce152fd6486726589441f50cc5ebf20e961ef
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-08-06 20:55:54 -0400

    Got something to display, but not formatted correctly

--- clock.js
+++ clock.js
@@ -1,9 +1,8 @@
-console.log("Working");
 let whiteTimeLeft = 300;
 let blackTimeLeft = 300;
 let whiteMove = true;
 let blackMove = false;
-let paused = true;
+let paused = false;
 let whiteMinutes = whiteTimeLeft/60;
 let whiteSeconds = whiteTimeLeft % 60;
 let blackMinutes = whiteTimeLeft/60;
@@ -11,19 +10,25 @@ let blackSeconds = whiteTimeLeft % 60;
 let whiteFraction = whiteTimeLeft % 600;
 let blackFraction = blackTimeLeft % 600;
 
-document.getElementById("white-minutes").innerHTML = parseInt(whiteMinutes);
-document.getElementById("white-seconds").innerHTML = parseInt(whiteSeconds);
-document.getElementById("black-minutes").innerHTML = parseInt(blackMinutes);
-//add ability to see two zeros here
-document.getElementById("black-seconds").innerHTML = parseInt(blackSeconds);
+let countdownDisplayer1 = document.querySelector('#first');
+let startTime1 = new Date().getTime();
+
+ 
+
+// document.getElementById("white-minutes").innerHTML = parseInt(whiteMinutes);
+// document.getElementById("white-seconds").innerHTML = parseInt(whiteSeconds);
+// document.getElementById("black-minutes").innerHTML = parseInt(blackMinutes);
+// //add ability to see two zeros here
+// document.getElementById("black-seconds").innerHTML = parseInt(blackSeconds);
 
 //add ability to toggle time
 //document.addEventListener("white-time-arrow-up", function())//continue here
 
-startButton = document.querySelector('.all');
+// startButton = document.querySelector('.all');
 
 
 window.addEventListener('keydown', function(e) {
+    //if the spacebar is pressed
     if(e.keyCode == 32 && e.target == document.body) {
       e.preventDefault();
       startButton.style.background = "red";
@@ -45,6 +50,7 @@ window.addEventListener('keydown', function(e) {
 
 
 window.addEventListener('keydown', function(e){
+    //if the p key is pressed
     if(e.keyCode == 80 &&e.target == this.document.body){
         e.preventDefault();
         if (e.keyCode == 80 && !paused){
@@ -57,12 +63,14 @@ window.addEventListener('keydown', function(e){
     }
 })
 
-setInterval(decreaser, 1000);
+//  setInterval(startDecreaser, 1000);
 
 
-function decreaser(){
+function startDecreaser(){
+    console.log("working");
 
     if (!paused){
+        console.log("Not paused");
 
     
         if(whiteMove){
@@ -74,8 +82,8 @@ function decreaser(){
                 whiteMinutes = whiteTimeLeft/60;
                 whiteSeconds = whiteTimeLeft % 60;
 
-                document.getElementById("white-minutes").innerHTML = parseInt(whiteMinutes);
-                document.getElementById("white-seconds").innerHTML = parseInt(whiteSeconds);
+                // document.getElementById("white-minutes").innerHTML = parseInt(whiteMinutes);
+                // document.getElementById("white-seconds").innerHTML = parseInt(whiteSeconds);
 
                 whiteTimeLeft--;
                 
@@ -120,5 +128,24 @@ function decreaser(){
     
     }
 }
+
+function getDisplayTime1(){
+    updatedTime1 = new Date().getTime();
+
+    //this will need to go in an else statement
+    difference1 = startTime1 - updatedTime1;
+
+    let hours1 = Math.floor((difference1 % (1000*60*60*24)) / (1000*60*60));
+    let minutes1 = Math.floor((difference1 % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds1 = Math.floor((difference1 % (1000 * 60)) / 1000);
+    let milliseconds1 = Math.floor(difference1 % (1000 * 60)) / 100;
+    hours1 = (hours1 < 10) ? "z" + hours1 : hours1;
+    minutes1 = (minutes1 < 10) ? "0" + minutes1 : minutes1;
+    seconds1 = (seconds1 < 10) ? "0" + seconds1 : seconds1;
+    milliseconds1 = (milliseconds1 < 100 ) ? (milliseconds1 < 10 ) ? "00" + milliseconds1 : "0" + milliseconds1 : milliseconds1;
+    countdownDisplayer1.innerHTML = hours1 + '::' + minutes1 + ':' + seconds1 + ':' + milliseconds1;
+}
+console.log('here');
+getDisplayTime1();
 
 
