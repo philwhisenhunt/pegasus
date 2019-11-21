97559d671bced88878e682eab32c1f46d812d1cc
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-11-20 22:36:12 -0500

    Now works kind of, but appears to add a few seconds when using the spacebar change feature

--- clock2.js
+++ clock2.js
@@ -43,8 +43,8 @@ window.addEventListener('keydown', function(e) {
 
 document.onreadystatechange = function () {
 
-        countdownTimer1.reset();
-        countdownTimer2.reset();
+    countdownTimer1.reset();
+    countdownTimer2.reset();
     
 }
 
@@ -55,11 +55,9 @@ const countdownTimer = {
     savedTime: null,
     preciseTimerSetting: 300000,
     timeRemaining: null,
-    //countdownDisplay: document.querySelector('#first'), //how do I get this to select one clock in one object, and another in another object?
-    timeRemaining: null,
     running: 0,
     pauseTime: null,
-    userSetMinutesPlayer: .03,
+    userSetMinutesPlayer: 5,
     firstTimerButton: document.getElementById('button1'), 
     secondTimerButton: document.getElementById('button2'), 
     minutes: null,
@@ -68,21 +66,15 @@ const countdownTimer = {
     difference: null,
 
     start: function(){
-      
-        // console.log('Startedeeee');
-        // console.log(this.paused);
+
         this.paused = 0; //no longer paused
         this.startTime = new Date().getTime();
-      
-        // countdownTimer2.pause();
         setInterval(() => this.getDisplayTime(), 1);
 
     },
 
 
     pause: function() {
-        // console.log('in pause'); 
-        // console.log(this.paused);
 
         if(this.paused == 0){
             //take a timestamp
@@ -100,21 +92,17 @@ const countdownTimer = {
 
  
     getDisplayTime: function(){
-        // console.log('Now in getDisplayTime');
-        // console.log(this.preciseTimerSetting);
+      
 
 
         if(this.paused == 0){
             
-            //console.log('in while');
-       
             this.updatedTime = new Date().getTime();
             if (this.savedTime){
-                //there is saved time, so that is why it is stopping
-                console.log('in saved time');
+              
                 this.difference = (this.updatedTime - this.startTime) + this.savedTime;
                 this.timeRemaining = (this.preciseTimerSetting - this.difference);
-                // return this.timeRemaining;//trying this
+                
 
                 this.minutes = Math.floor((this.timeRemaining % (1000 * 60 * 60)) / (1000*60));
                 this.seconds = Math.floor((this.timeRemaining % (1000 * 60)) / 1000);
@@ -126,15 +114,10 @@ const countdownTimer = {
         
             }
             else {
-                //console.log('Now in the else of getDisplaytime');
                 
                 this.difference = this.updatedTime - this.startTime;
                 this.savedTime = this.difference;
                 this.timeRemaining = this.preciseTimerSetting - this.difference;
-
-              
-            
-                
                 this.minutes = Math.floor((this.timeRemaining % (1000 * 60 * 60)) / (1000*60));
                 this.seconds = Math.floor((this.timeRemaining % (1000 * 60)) / 1000);
                 this.milliseconds = Math.floor((this.timeRemaining % (1000* 60)) /100);
@@ -142,14 +125,10 @@ const countdownTimer = {
                 this.minutes = (this.minutes < 10) ? "0" + this.minutes : this.minutes;
                 this.seconds = (this.seconds < 10) ? "0" + this.seconds : this.seconds;
                 this.milliseconds = (this.milliseconds < 10) ? "0" + this.milliseconds : this.milliseconds;
-                //console.log("This time " + minutes + ":" + seconds + ":" + milliseconds);
-                //countdownDisplay.innerHTML = minutes + ":" + seconds + ":" + milliseconds;
-
-            
             }
             
         }
-        //console.log('Three');
+      
         if(this.timeRemaining <= 0){
             this.countdownDisplay.innerHTML = "00:00:00";        
         }
@@ -161,20 +140,14 @@ const countdownTimer = {
     },
 
     reset: function(){
-        //clearInterval(timeInterval1);
+       
         this.savedTime = 0;
         this.difference = 0;
         this.paused = 1;
         this.running = 0;
         this.minutes = 0;
         this.preciseTimerSetting = this.userSetMinutesPlayer * 60 * 1000;
-        console.log(this.userSetMinutesPlayer);
-        console.log(this.preciseTimerSetting);
-
         this.timeRemaining = this.preciseTimerSetting - this.difference;
-    
-    
-    
         this.minutes = Math.floor((this.timeRemaining % (1000 * 60 * 60)) / (1000*60));
         this.seconds = Math.floor((this.timeRemaining % (1000 * 60)) / 1000);
         this.milliseconds = Math.floor((this.timeRemaining % (1000* 60)) /100);
