7849d9a8fed0980fc2585b745ac8b9ad00e0d9ae
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-12-02 17:57:41 -0500

    Finished line by line attentive copy

--- assist-follow.js
+++ assist-follow.js
@@ -63,7 +63,10 @@ function createTimer(name, domElement){
             savedTime = difference;
 
             const minutes = Math.floor((timeRemaining % (1000*60*60)) / (1000*60));
-        
+            const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+            const seconds = Math.floor((timeRemaining % (1000 * 60)) / 100);
+
+            printDisplay(toHumanReadableTime(minutes, seconds, milliseconds));
         }
     }
 
